eba2e267d85cfdd41b522ec54de96425dccc5c36
Ejz9 <114949726+Ejz9@users.noreply.github.com>
2023-05-03 15:16:22 -0500

    !

---
+++ src/main/java/com/not/monopoly/Controller.java
@@ -0,0 +1,25 @@
+package com.not.monopoly;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class Controller {
+    @FXML
+    ImageView playButton;
+
+    public void handlePlayButton() throws IOException {
+
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("main-board.fxml")));
+        Stage window = (Stage) playButton.getScene().getWindow();
+        window.setScene(new Scene(root, 800, 800));
+
+
+    }
+}
