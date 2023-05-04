4a1b273201ad4e58947aa2a3f9d42ccc24a8d1a8
Ejz9 <114949726+Ejz9@users.noreply.github.com>
2023-05-04 17:00:29 -0500

    !

--- src/main/java/com/not/monopoly/PlayerCreationController.java
+++ src/main/java/com/not/monopoly/PlayerCreationController.java
@@ -4,15 +4,15 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.image.ImageView;
+import javafx.scene.control.*;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.util.Objects;
 
 public class PlayerCreationController {
-    public static int playerCount;
+    int playerCount;
     @FXML
     Button backButton;
 
@@ -54,7 +54,35 @@ public class PlayerCreationController {
 
     @FXML
     Button confirmButton;
-    public void handleConfirmButton() throws IOException {
+    @FXML
+    TextField playerOneName;
+    @FXML
+    CheckBox playerOneCheck;
+    @FXML
+    String playerOne;
+    @FXML
+    TextField playerTwoName;
+    @FXML
+    CheckBox playerTwoCheck;
+    @FXML
+    String playerTwo;
+    @FXML
+    TextField playerThreeName;
+    @FXML
+    CheckBox playerThreeCheck;
+    @FXML
+    String playerThree;
+    @FXML
+    TextField playerFourName;
+    @FXML
+    CheckBox playerFourCheck;
+    @FXML
+    String playerFour;
+    @FXML
+    Label countQuestion;
+    @FXML
+    Label nameQuestion;
+    public void handleConfirmButton() {
         if (twoPlayerButton.isDisabled()){
             playerCount = 2;
         } else if (threePlayerButton.isDisabled()) {
@@ -62,10 +90,7 @@ public class PlayerCreationController {
         } else if (fourPlayerButton.isDisabled()) {
             playerCount = 4;
         }
-        resetScene();
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("main-board.fxml")));
-        Stage window = (Stage) backButton.getScene().getWindow();
-        window.setScene(new Scene(root, 1280, 720));
+        setPLayerNameScene();
     }
 
     @FXML
@@ -75,11 +100,126 @@ public class PlayerCreationController {
         resetScene();
     }
 
+    @FXML
+    Button proceedToGameButton;
+    public void handlePlayerCreation() {
+        if (playerOneCheck.isSelected()) {
+            playerOneName.setEditable(false);
+        } else if (playerTwoCheck.isSelected()) {
+            playerTwoName.setEditable(false);
+        } else if (playerThreeCheck.isSelected()) {
+            playerThreeName.setEditable(false);
+        } else if (playerFourCheck.isSelected()) {
+            playerFourName.setEditable(false);
+        } else {
+            playerOneName.setEditable(true);
+            playerTwoName.setEditable(true);
+            playerThreeName.setEditable(true);
+            playerFourName.setEditable(true);
+        }
+        switch (playerCount) {
+            case 2 -> {
+                if (playerOneCheck.isSelected() && playerTwoCheck.isSelected()) {
+                    proceedToGameButton.setVisible(true);
+                    playerOne = String.valueOf(playerOneName);
+                    playerTwo = String.valueOf(playerTwoName);
+                }
+            }
+            case 3 -> {
+                if (playerOneCheck.isSelected() && playerTwoCheck.isSelected() && playerThreeCheck.isSelected()) {
+                    proceedToGameButton.setVisible(true);
+                    playerOne = String.valueOf(playerOneName);
+                    playerTwo = String.valueOf(playerTwoName);
+                    playerThree = String.valueOf(playerThreeName);
+                }
+            }
+            case 4 -> {
+                if (playerOneCheck.isSelected() && playerTwoCheck.isSelected() && playerThreeCheck.isSelected() && playerFourCheck.isSelected()) {
+                    proceedToGameButton.setVisible(true);
+                    playerOne = String.valueOf(playerOneName);
+                    playerTwo = String.valueOf(playerTwoName);
+                    playerThree = String.valueOf(playerThreeName);
+                    playerFour = String.valueOf(playerFourName);
+                }
+            }
+        }
+    }
+
+    public void handleCheckOne() {
+        handleChecked(playerOneCheck);
+    }
+
+    public void handleCheckTwo() {
+        handleChecked(playerTwoCheck);
+    }
+
+    public void handleCheckThree() {
+        handleChecked(playerThreeCheck);
+    }
+
+    public void handleCheckFour() {
+        handleChecked(playerFourCheck);
+    }
+
+    public void handleChecked(CheckBox checkbox) {
+        if (checkbox.isSelected()) {
+            checkbox.setSelected(false);
+        }
+        checkbox.setSelected(true);
+        handlePlayerCreation();
+    }
+
     public void resetScene() {
         twoPlayerButton.setDisable(false);
+        twoPlayerButton.setVisible(true);
         threePlayerButton.setDisable(false);
+        threePlayerButton.setVisible(true);
         fourPlayerButton.setDisable(false);
+        fourPlayerButton.setVisible(true);
         cancelButton.setVisible(false);
         confirmButton.setVisible(false);
+        playerNamePane.setDisable(false);
+        playerNamePane.setVisible(false);
+        playerThreeName.setVisible(false);
+        playerThreeCheck.setVisible(false);
+        playerFourName.setVisible(false);
+        playerFourCheck.setVisible(false);
+
+    }
+
+    @FXML
+    Pane playerCountPane;
+    @FXML
+    Pane playerNamePane;
+    public void setPLayerNameScene() {
+        playerCountPane.setDisable(true);
+        playerCountPane.setVisible(false);
+
+        if (playerCount == 2) {
+            playerOneName.setVisible(true);
+            playerOneCheck.setVisible(true);
+            playerTwoName.setVisible(true);
+            playerTwoCheck.setVisible(true);
+
+        } else if (playerCount == 3) {
+            playerOneName.setVisible(true);
+            playerOneCheck.setVisible(true);
+            playerTwoName.setVisible(true);
+            playerTwoCheck.setVisible(true);
+            playerThreeName.setVisible(true);
+            playerThreeCheck.setVisible(true);
+        } else if (playerCount == 4) {
+            playerOneName.setVisible(true);
+            playerOneCheck.setVisible(true);
+            playerTwoName.setVisible(true);
+            playerTwoCheck.setVisible(true);
+            playerThreeName.setVisible(true);
+            playerThreeCheck.setVisible(true);
+            playerFourName.setVisible(true);
+            playerFourCheck.setVisible(true);
+        }
+        playerNamePane.setVisible(true);
+        playerNamePane.setDisable(false);
+        confirmButton.setTooltip(new Tooltip("Proceed to the the world of Monopoly!"));
     }
 }
