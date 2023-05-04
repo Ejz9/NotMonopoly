e74016068117c1cfc1d0e7a0b8e4eaeb03d9618d
Ashton <fordashton3@gmail.com>
2023-05-04 13:54:03 -0500

    Added behavior for menu buttons

---
+++ src/main/java/com/not/monopoly/GameController.java
@@ -0,0 +1,20 @@
+package com.not.monopoly;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.image.ImageView;
+
+public class GameController {
+	public int doubleRoll = 0;
+
+	@FXML
+	ImageView rollButton;
+
+	@FXML
+	ImageView managePropertyButton;
+
+	public void	handleRollButton(){
+
+	}
+
+}
