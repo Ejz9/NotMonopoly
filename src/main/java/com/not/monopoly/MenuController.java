e9a0703a8ba0a9eff86b2c3451d79b61dd0997f5
Ashton <fordashton3@gmail.com>
2023-05-04 12:54:13 -0500

    Added behavior for menu buttons

--- src/main/java/com/not/monopoly/MenuController.java
+++ src/main/java/com/not/monopoly/MenuController.java
@@ -10,7 +10,7 @@ import javafx.stage.Stage;
 import java.io.IOException;
 import java.util.Objects;
 
-public class Controller {
+public class MenuController {
     @FXML
     ImageView playButton;
 
